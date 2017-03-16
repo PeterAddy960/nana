67e83293feab75072c4258e426d5aabdccc44bbe
Peter Addy <peteraddy96@gmail.com>
2017-03-16 09:02:12 -0700

    Added Endian object to deal with the Endian of a OS

---
+++ include/nana/detail/endian.hpp
@@ -0,0 +1,28 @@
+#ifndef NANA_DETAIL_ENDIAN_HPP
+#define NANA_DETAIL_ENDIAN_HPP
+#include <algorithm>
+
+namespace nana
+{
+	namespace detail
+	{
+		class Endian
+		{
+			bool LittleEndian;
+		public:
+			Endian();
+			template<typename T> static Reverse(T &Var)
+			{
+				unsigned char* Bytes = (unsigned char*)&Var;
+				for(size_t i = 0,j = sizeof(T) - 1; i < j; i++,j--)
+				{
+					std::swap(Bytes[i],Bytes[j]);
+				}
+			}
+			bool IsLittleEndian();
+			bool IsBigEndian();
+		};
+	};
+};
+
+#endif
